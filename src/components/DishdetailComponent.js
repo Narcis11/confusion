ee8d4d18db73201dca282bad90f214a5f9ae04ec
Narcis11 <narcismihai11@gmail.com>
2018-11-17 08:33:42 +0100

    Refactoring to functional components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 
-class DishDetail extends Component {
-
-      renderComment(selectedDish) {
-        //console.log('In renderComment, comments is: ' + comments);
-        if (selectedDish != null) { 
-            const commentsSection = selectedDish.comments.map(comment => {
+   function RenderComments({comments}) {
+        if (comments != null) {
+            console.log('In renderComment, comments is: ' + comments.name); 
+            const commentsSection = comments.map(comment => {
                 //console.log('key is: ' + comment.comment);
                 return (
                     <div> 
                         <li className="mb-3" key={comment.id}>
                             <p className="mb-0">{comment.comment}</p>
                             <p>{comment.author}</p>
+                            <p>{new Intl.DateTimeFormat('ro-RO', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                         </li>
                     </div>
                 );
@@ -26,15 +25,20 @@ class DishDetail extends Component {
          )
     }
         else {
+            console.log('In renderComment, comments is null');
             return (
                 <div></div>//nothing will be rendered on the screen
               );
         }
         
-    }
+    };
+    
+    
 
-    renderDish(dish) {
+    function RenderDish({dish}) {
+        
         if (dish != null) {
+          console.log('In renderDish, dish is: ' + dish.name);
           return (
             <Card>
               <CardImg width="100%" object src={dish.image} alt={dish.name} />
@@ -46,24 +50,34 @@ class DishDetail extends Component {
           );
         }
         else {
-          
+          console.log('In renderDish, Dish is null');
+          return(
+              <div> </div>
+          )
         }
       }
 
-      render() {
+      const DishDetail = (props) => {
+        if (props.dish != null) {
         return (
-            <div className="row">
-                <div className="col-6 col-md-5 m-1">
-                    {this.renderDish(this.props.selectedDish)}
+            <div className="container">
+                <div className="row">
+                    <div className="col-6 col-md-5 m-1">
+                        <RenderDish dish={props.dish}/>
+                    </div>
+                    <div className="col-6 col-md-5 m-1">
+                        <RenderComments comments={props.dish.comments}/>
+                    </div>
                 </div>
-                <div className="col-6 col-md-5 m-1">
-                    {this.renderComment(this.props.selectedDish)}  
-                </div>
-                
             </div>
         );
-    }
-}
+        }
+        else {
+            return (
+                null
+            );
+        }
+      }
 
 
 
